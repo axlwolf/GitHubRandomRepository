00cb43c02d0ad0a9216b62e0b8bdd37a8b32095e
Axel Lanuza <rojasf@ae.com>
2024-12-16 16:33:38 -0600

    1. Project completed

---
+++ README.md
@@ -0,0 +1,59 @@
+## Image Grid Layout
+
+Roadmap project link: https://roadmap.sh/projects/github-random-repo/solutions?u=
+
+Roadmap project: https://roadmap.sh/projects/github-random-repo
+
+Github link: https://github.com/axlwolf/GitHubRandomRepository
+
+Github pages link: https://axlwolf.github.io/GitHubRandomRepository/
+
+This project is designed to introduce you to working with external APIs, handling asynchronous requests, and managing different UI states with JavaScript.
+
+You will create a GitHub repository finder that allows users to select a programming language from a dropdown menu. The app will then use the GitHub Repository Search API to fetch and display a random repository that matches the selected language. The displayed information should include the repository name, description, number of stars, forks, and open issues. Users can fetch another random repository with a button click.
+
+[![GitHub Random Repository](https://assets.roadmap.sh/guest/github-repo-finder-n2qz4.png)](https://assets.roadmap.sh/guest/github-repo-finder-n2qz4.png)
+
+The application should handle loading, empty, and error states effectively. After successfully fetching a repository, a "Refresh" button should appear to allow users to get another random repository.
+
+Here are the links to the resources you will need for this project:
+
+- [GitHub Repository Search API](https://docs.github.com/en/rest/reference/search#search-repositories)
+- [Programming Language Data](https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json)
+
+This project will help you practice API integration, managing asynchronous data, and enhancing user experience with responsive UI states.
+
+### Get started
+
+```
+  git clone https://github.com/axlwolf/rm-DatepickerUI.git my_project
+  cd my_project
+  npm i
+  npm start
+```
+
+### Put your files into /src folder.
+
+Don't forget add to index.html this line:
+```
+  <script type="text/javascript" src="../build/app.js"></script>
+```
+
+### For build
+
+```
+  npm run build
+```
+
+### Features
+
+- Lightweight, fast opened.
+- Webpack livereload separated config
+- Html5/Css3/JS(es6/vanilla)/images(png, jpg, gif, svg) optimization/minify.
+- Include 5 pure css media queries points and best normalize css practices.
+- Include all you needed html meta tags.
+- ES6 and browserify syntax support. (require, arrow func, let const etc...).
+
+### 2020 updates
+
+- SASS support (thanks @patrikniebur)
---
+++ src/js/gh-random-repository.js
@@ -0,0 +1,135 @@
+import { CustomDropdown } from "./custom-dropdown";
+
+export const GhRandomRepository = (() => {
+  const repoContainer = document.querySelector("#repo-container");
+  const refreshButton = document.getElementById("refresh-button");
+  refreshButton.textContent = "Refresh";
+  refreshButton.style.display = "none"; // Initially hidden
+  // repoContainer.after(refreshButton);
+
+  let dropdown;
+  let currentRepo;
+  const GITHUB_TOKEN = process.env.GH_API_KEY;
+
+  const init = async () => {
+    dropdown = new CustomDropdown("#language-dropdown", {
+      defaultValue: "Select a language",
+      items: [],
+    });
+
+    // Escuchar el evento 'change' en el dropdown container:
+    document
+      .getElementById("language-dropdown")
+      .addEventListener("change", (event) => {
+        console.log("Language changed to:", event.detail.value); // Aquí haces lo que necesites con el nuevo lenguaje.
+        fetchRandomRepo(); // Recargar repositorios al cambiar el lenguaje.
+      });
+
+    refreshButton.addEventListener("click", fetchRandomRepo);
+    await getLanguages();
+    // await fetchRandomRepo(); // Fetch initial repo
+  };
+
+  const getLanguages = async () => {
+    const languageApiURL =
+      "https://raw.githubusercontent.com/kamranahmedse/githunt/master/src/components/filters/language-filter/languages.json";
+    try {
+      const response = await fetch(languageApiURL);
+      const data = await response.json();
+
+      const languages = data
+        .map((item, index) => (index !== 0 ? item.title : null))
+        .filter(Boolean);
+
+      dropdown.options.items = languages;
+      dropdown.render();
+    } catch (error) {
+      console.error("Error fetching languages:", error);
+      displayError("Error loading languages.");
+    }
+  };
+
+  const fetchRandomRepo = async () => {
+    const selectedLanguage =
+      dropdown.input.value === dropdown.options.defaultValue
+        ? "javascript"
+        : `language:${dropdown.input.value}`; // Use "javascript" as default
+    console.log({ selectedLanguage });
+    const apiUrl = `https://api.github.com/search/repositories?q=${selectedLanguage}&sort=stars&order=desc`;
+
+    displayLoading();
+
+    try {
+      const response = await fetch(apiUrl, {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`, // Add token to headers
+        },
+      });
+      if (!response.ok) {
+        if (response.status === 403) {
+          throw new Error(
+            "GitHub API rate limit exceeded. Please try again later."
+          );
+        }
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data.items.length === 0) {
+        displayEmpty();
+      } else {
+        const randomIndex = Math.floor(Math.random() * data.items.length);
+        currentRepo = data.items[randomIndex];
+        displayRepo(currentRepo);
+      }
+    } catch (error) {
+      console.error("Error fetching respository:", error);
+      displayError(error.message); // Display the specific error message
+    } finally {
+      //hideLoading();
+    }
+  };
+
+  const displayLoading = () => {
+    repoContainer.innerHTML = "<p>Loading repository...</p>";
+    refreshButton.style.display = "none";
+  };
+
+  const displayEmpty = () => {
+    repoContainer.innerHTML = "<p>No repositories found for this language.</p>";
+    refreshButton.style.display = "block";
+  };
+
+  const displayError = (message) => {
+    repoContainer.innerHTML = `<p style="color: red;">${message}</p>`;
+    refreshButton.style.display = "block";
+  };
+
+  const displayRepo = (repo) => {
+    // Construct icon HTML based on language
+    let iconHTML = "";
+    if (repo.language) {
+      iconHTML = `<i class="devicon-${repo.language.toLowerCase()}-plain"></i>`; // Use lowercase for consistency
+    }
+
+    repoContainer.innerHTML = `
+    <h2><a href="${repo.html_url}" target="_blank">${repo.full_name}</a></h2>
+    <p>${repo.description || "No description provided."}</p>
+    <div class="repo-info">
+      <small> ${iconHTML} ${repo.language || "Not specified"}</small>
+       <small><i class="fa fa-star"></i> ${repo.stargazers_count}</small>
+            <small><i class="fa fa-code-branch"></i> ${repo.forks_count}</small>
+            <small><i class="fa fa-exclamation-circle"></i> ${
+              repo.open_issues_count || "Not available"
+            }</small>
+  `;
+    refreshButton.style.display = "block";
+  };
+
+  // const hideLoading = () => {
+  //   repoContainer.innerHTML = ""; // Clear loading message
+  // };
+
+  return { init };
+})();
